5d72aac2656a0e2cc7cd13bcd79db0d18d89cbcf
XanderFerrero <xanderferrero@gmail.com>
2023-08-12 23:48:52 +0800

    proxy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':"http://localhost:3001"
+      '/api':"https://mern-task-recorder-api.onrender.com"
     }
   },
   plugins: [react()],
