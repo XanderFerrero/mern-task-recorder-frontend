924a3dc7dce4bef46685e6ff05d84ebd7fc90207
XanderFerrero <xanderferrero@gmail.com>
2023-08-13 00:05:20 +0800

    s

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:"https://mern-task-recorder-api.onrender.com",
-        changeOrigin: true,
-           secure: false,      
-           ws: true,
-      }
-    }
-  },
+  // server:{
+  //   proxy:{
+  //     '/api':{
+  //       target:"https://mern-task-recorder-api.onrender.com",
+  //       changeOrigin: true,
+  //          secure: false,      
+  //          ws: true,
+  //     }
+  //   }
+  // },
   plugins: [react()],
 })
